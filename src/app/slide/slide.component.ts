01b273064c34395f006a2b8c000d630a57e353ec
da-the-dev <leshiktk@me.com>
2021-10-22 15:45:09 +0300

    Added slide

--- src/app/slide/slide.component.ts
+++ src/app/slide/slide.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
 
 @Component({
   selector: 'app-slide',
@@ -9,7 +11,18 @@ export class SlideComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit() {
+    gsap.registerPlugin(ScrollTrigger);
 
+    gsap.utils.toArray("section").forEach((section: any, i) => {
+      section.style.backgroundImage = `url(https://picsum.photos/${innerWidth}/${innerHeight}?random=${i})`;
+      gsap.to(section, {
+        scrollTrigger: {
+          trigger: section,
+          pin: true,
+          pinSpacing: false
+        }
+      });
+    });
+  }
 }
